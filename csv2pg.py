9fe61786b40f16ee9abccafe354715859d555dcd
Ben Lis <ben@leismart.com>
2018-11-10 11:05:38 -0500

    Added opton to support copy command options.

--- csv2pg.py
+++ csv2pg.py
@@ -14,6 +14,8 @@ parser.add_argument('-d', '--ddl',
                     help='filename of create table statement')
 parser.add_argument('-p', '--pk',
                     help='system generated primary key column')
+parser.add_argument('-o', '--optcp',
+                    help='copy command options')
 args = parser.parse_args()
 
 
@@ -33,8 +35,17 @@ def gen_table_ddl(tabname, pkcol, colnames):
         ddl = ddl + "\t {} \t serial,\n".format(pkcol)
     for c in colnames:
         ddl = ddl + "\t {} \t text,\n".format(c)
-    ddl = ddl[:-2] + "\n);"
-    return(ddl)
+    ddl = ddl[:-2] + "\n);\n"
+    return ddl
+
+
+def gen_copy_cmd(tabname, colnames, filename, optcp):
+    """Generate the copy command for the CSV file."""
+    ddl = "\copy {}(".format(tabname)
+    for c in colnames:
+        ddl = ddl + "{}, ".format(c)
+    ddl = ddl[:-2] + ") from '{}' {};\n".format(filename, optcp)
+    return ddl
 
 
 table_name = args.table or get_table_name(args.filename)
@@ -48,10 +59,13 @@ except FileNotFoundError:
 # read the header
 # iconv -f ISO-8859-1 -t UTF-8 X.csv > X_utf8.csv
 try:
-    create_table = gen_table_ddl(table_name, args.pk, csvreader.__next__())
+    csvheader = csvreader.__next__()
+    create_table = gen_table_ddl(table_name, args.pk, csvheader)
+    copy_cmd = gen_copy_cmd(table_name, csvheader, args.filename, args.optcp)
 except UnicodeDecodeError:
     print("File {} must be UTF-8 encoded. Try iconv.".format(args.filename))
     raise SystemExit
 # write the DDL to a file
 ddl_file = open("{}.sql".format(args.ddl or table_name), "w+")
 ddl_file.write(create_table)
+ddl_file.write(copy_cmd)
