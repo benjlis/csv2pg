9a3fbfcebab41c70601e4f9582a105fe759293a1
Ben Lis <ben@leismart.com>
2019-01-13 11:36:53 -0500

    Add automatic file type conversion and BOM removal. Add default COPY options.

--- csv2pg.py
+++ csv2pg.py
@@ -2,6 +2,7 @@
 
 import argparse
 import csv
+import magic
 
 # process command line arguments
 parser = argparse.ArgumentParser(description='Loads a CSV file into \
@@ -17,6 +18,22 @@ parser.add_argument('-p', '--pk',
 parser.add_argument('-o', '--optcp',
                     help='copy command options')
 args = parser.parse_args()
+
+
+def convert_utf8(filename, filetype):
+    """Convert filename to UTF-8 without BOM."""
+    print("File {} appears to be in {} format.".format(filename, filetype))
+    print("Converting to utf-8 without bom.")
+    if filetype == 'utf-8':             # to handle BOM
+        filetype = 'utf-8-sig'
+    with open(filename, "rb") as fin:
+        text = fin.read()
+    text = text.decode(filetype).encode('utf-8')
+    utf8_out = 'csv2pg-' + filename
+    with open(utf8_out, "wb") as fou:
+        fou.write(text)
+    print("Converted file: {}".format(utf8_out))
+    return utf8_out
 
 
 def get_table_name(filename):
@@ -49,23 +66,25 @@ def gen_copy_cmd(tabname, colnames, filename, optcp):
 
 
 table_name = args.table or get_table_name(args.filename)
-# open the CSV file for reading
+# determine the file type and generate a UTF-8 version of file if necessary
+f = magic.Magic(mime_encoding="True")
 try:
-    csvfile = open(args.filename, newline='')
-    csvreader = csv.reader(csvfile)
+    file_format = f.from_file(args.filename)
 except FileNotFoundError:
     print("File {} not found".format(args.filename))
     raise SystemExit
-# read the header
-# iconv -f ISO-8859-1 -t UTF-8 X.csv > X_utf8.csv
-try:
-    csvheader = csvreader.__next__()
-    create_table = gen_table_ddl(table_name, args.pk, csvheader)
-    copy_cmd = gen_copy_cmd(table_name, csvheader, args.filename, args.optcp)
-except UnicodeDecodeError:
-    print("File {} must be UTF-8 encoded. Try iconv.".format(args.filename))
-    raise SystemExit
+# TBD - handle for Postgres DBs where character set is not UTF-8
+# TBD - better testing for BOM, better use of space.
+csvfilename = convert_utf8(args.filename, file_format)
+csvfile = open(csvfilename, newline='')
+csvreader = csv.reader(csvfile)
+csvheader = csvreader.__next__()                    # read the header
+create_table = gen_table_ddl(table_name, args.pk, csvheader)
+copy_opts = args.optcp or "DELIMITER ',' CSV HEADER"
+copy_cmd = gen_copy_cmd(table_name, csvheader, csvfilename, copy_opts)
 # write the DDL to a file
-ddl_file = open("{}.sql".format(args.ddl or table_name), "w+")
+ddl_file_name = "{}.sql".format(args.ddl or table_name)
+ddl_file = open(ddl_file_name, "w+")
 ddl_file.write(create_table)
 ddl_file.write(copy_cmd)
+print("Generated SQL file: {}".format(ddl_file_name))
